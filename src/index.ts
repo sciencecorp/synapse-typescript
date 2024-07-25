dd910bb6821be2973db3c6d3deb4072210894ebf
Antonia Elsen <antoniaelsen@gmail.com>
2024-07-25 13:54:57 -0700

    fix: correctly export enums

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import ChannelMask from "./channel_mask";
 import Config from "./config";
 import Device from "./device";
 import Node from "./node";
-export type * from "./api";
+export * from "./api/synapse";
 
 export { ChannelMask, Config, Device, Node };
