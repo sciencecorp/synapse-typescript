d2654adcdb9fac6e236c69259d088929baffdd2e
Antonia Elsen <antoniaelsen@gmail.com>
2024-07-30 17:27:28 -0700

    feat: export types and enums from root index

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -15,7 +15,12 @@ proto-loader-gen-types  \
     ${PROTOS}
 
 # Create barrel file for types
-rm -f ${PROTO_OUT_DIR}/index.ts && touch ${PROTO_OUT_DIR}/index.ts
+TYPES_FILE=${PROTO_OUT_DIR}/types.ts
+rm -f ${TYPES_FILE} && touch ${TYPES_FILE}
 for file in $(find ${PROTO_OUT_DIR}/synapse -name '*.ts' | sed "s|${PROTO_OUT_DIR}/synapse/||" | sed "s|.ts$||"); do
-    echo "export * from './synapse/${file}';" >> ${PROTO_OUT_DIR}/index.ts
+    # check that there's no grpc-js import
+    if grep -q "@grpc/grpc-js" ${PROTO_OUT_DIR}/synapse/${file}.ts; then
+        continue
+    fi
+    echo "export * from './synapse/${file}';" >> ${TYPES_FILE}
 done
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import ChannelMask from "./channel_mask";
 import Config from "./config";
 import Device from "./device";
 import Node from "./node";
-export * from "./api/synapse";
+export * from "./api/types";
 
 export { ChannelMask, Config, Device, Node };
