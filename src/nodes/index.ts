8440c0993310e787317de83da73d1f10276cef4b
Antonia Elsen <antoniaelsen@gmail.com>
2025-02-05 17:36:16 -0800

    feat: add BroadbandSource, SpikeSource nodes

--- src/nodes/index.ts
+++ src/nodes/index.ts
@@ -1,8 +1,10 @@
-export { default as ElectricalBroadband } from "./electrical_broadband";
+export { default as BroadbandSource } from "./broadband_source";
 export { default as ElectricalStimulation } from "./electrical_stimulation";
-export { default as OpticalBroadband } from "./optical_broadband";
 export { default as OpticalStimulation } from "./optical_stimulation";
 export { default as SpectralFilter } from "./spectral_filter";
 export { default as SpikeDetect } from "./spike_detect";
+export { default as SpikeSource } from "./spike_source";
 export { default as StreamIn } from "./stream_in";
 export { default as StreamOut } from "./stream_out";
+
+export type * from "../api/api";
