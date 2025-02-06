1c79a6c3dbf314ed7fd0d077d5f5f85796b09f2e
Antonia Elsen <antoniaelsen@gmail.com>
2025-02-05 17:37:03 -0800

    feat: update discovery implementation

--- src/utils/discover.ts
+++ src/utils/discover.ts
@@ -1,81 +1,62 @@
 import dgram from "dgram";
 
-const kDefaultTimeout = 3000;
+const BROADCAST_PORT = 6470;
 
-interface DiscoverArgs {
+interface DeviceInfo {
   host: string;
   port: number;
-  timeoutMs?: number;
-}
-
-class DeviceAdvertisement {
-  host: string;
-  port: number;
-  serial: string;
   capability: string;
   name: string;
+  serial: string;
 }
 
-const kDefaultDiscoverArgs: DiscoverArgs = {
-  host: "224.0.0.245",
-  port: 6470,
-  timeoutMs: kDefaultTimeout,
-};
-
-export const discover = (args = kDefaultDiscoverArgs): Promise<DeviceAdvertisement[]> => {
+export const discover = (timeoutMs = 1000): Promise<DeviceInfo[]> => {
   return new Promise((resolve, reject) => {
-    const devices: DeviceAdvertisement[] = [];
+    const devices: DeviceInfo[] = [];
+    const socket = dgram.createSocket({ type: "udp4", reuseAddr: true });
 
-    const socket = dgram.createSocket("udp4");
+    socket.bind(BROADCAST_PORT);
 
     socket.on("message", (msg: Buffer, rinfo: dgram.RemoteInfo) => {
       const message = msg.toString("ascii");
       const split = message.split(" ");
 
-      if (split.length < 5) {
+      if (split[0] !== "ID" || split.length !== 5) {
         return;
       }
 
-      if (split[0] !== "ID") {
-        return;
-      }
-
-      const host = rinfo.address;
-      const [, serial, capability, portstr, name] = split;
-      const port = parseInt(portstr);
+      const [_, serial, capability, portStr, name] = split;
+      const port = parseInt(portStr);
       if (isNaN(port)) {
         return;
       }
 
-      const service = capability.split(/(\d+)/)[0];
-      if (service !== "SYN") {
-        return;
-      }
-
-      devices.push({
-        host,
+      const deviceInfo: DeviceInfo = {
+        host: rinfo.address,
         port,
-        serial,
         capability,
         name,
-      });
-    });
+        serial,
+      };
 
-    const payload = Buffer.from(`DISCOVER`, "ascii");
-
-    socket.send(payload, 0, payload.length, args.port, args.host, () => {
-      if (!args.timeoutMs) {
-        return;
+      if (
+        !devices.find((d) => d.host === deviceInfo.host && d.port === deviceInfo.port && d.serial === deviceInfo.serial)
+      ) {
+        devices.push(deviceInfo);
       }
-      setTimeout(() => socket.close(), kDefaultTimeout);
     });
 
     socket.on("error", (err: Error) => {
+      socket.close();
       reject(err);
     });
 
     socket.on("close", () => {
       resolve(devices);
     });
+
+    setTimeout(() => {
+      socket.close();
+    }, timeoutMs);
   });
 };
