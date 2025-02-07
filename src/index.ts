0f335e93a81028674ea3a9cd45885f5594ace67a
Antonia Elsen <antoniaelsen@gmail.com>
2025-02-07 13:14:34 -0800

    chore: fix dist directory

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as Config } from "./config";
 export { default as Device, CallOptions } from "./device";
 export * from "./nodes";
-export { synapse } from "./api/api";
+export * from "./api/api";
