73060b379e932eaf35c85048382c92c716578756
Antonia Elsen <antoniaelsen@gmail.com>
2025-02-28 12:41:26 -0800

    feat: add types file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as Config } from "./config";
 export { default as Device, CallOptions } from "./device";
 export * from "./nodes";
-export * from "./api/api";
+export * from "./types";
